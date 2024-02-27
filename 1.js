34567f27bc299362af6a00fb50aa0c69e867fbf3
DiRaAL <locosporalgo@gmail.com>
2024-02-26 21:28:26 -0800

    primer paso

--- 1.js
+++ 1.js
@@ -1,1 +1,10 @@
-""
+function numRandom() {
+  let randomNumber;
+  do {
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+  } while (randomNumber > 100);
+  return randomNumber;
+}
+
+console.log(numRandom());
+
---
+++ 2.js
@@ -0,0 +1,9 @@
+function genRandom() {
+Math.floor(math.random()*100)+1
+
+}
+while (genRandom > 100);
+return genRandom;
+
+console.log (genRandom)
+  
